25f9dd6282eee2202f08bb3159928c5f4e0f36b8
Saymon Tavares <saymon.tavares@satc.edu.br>
2021-04-12 17:05:17 -0300

    first commit

---
+++ Compilador.class.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Class Analisador Léxico
+ * @author Saymon Tavares
+ * @author Matheus
+ */
+class Compilador {
+
+    private $code;
+    private $tokens;
+    private $tableSerial;
+    private $reservedWords = [
+        1   => 'programa',
+        2   => '{',
+        3   => 'funcao',
+        4   => 'inteiro',
+        5   => 'escreva',
+        6   => '(',
+        7   => ')',
+        8   => 'leia',
+        9   => '=',
+        10  => '/',
+        11  => '%',
+        12  => 'escreva'
+    ];
+
+    public function __construct($code)
+    {
+        $this->code = $code;
+    }
+
+
+    public function createTokens()
+    {
+        $this->tokens = str_replace(['  ', "\t"], '', $this->code);
+        $this->tokens = explode("\n", $this->tokens);
+        foreach ($this->tokens as $k => $v) $this->tokens[$k] = trim($v);
+        $this->tokens = array_filter($this->tokens);
+        return $this->tokens;
+    }
+
+    public function createTableSerial()
+    {
+        $tokens = $this->createTokens();
+        foreach ($tokens as $k => $v) {
+
+            echo "<pre>";
+            print_r ($v);
+            echo "</pre>";
+
+        }
+    }
+
+    public function compilar()
+    {
+        return $this->createTableSerial();
+    }
+
+}
