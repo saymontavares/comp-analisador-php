d25bc5b0d6f5189c0a3e9e2cd50fc70d59770458
Saymon Tavares <saymon.tavares@satc.edu.br>
2021-04-13 10:41:18 -0300

    Analisador Léxico

--- Compilador.class.php
+++ Compilador.class.php
@@ -21,8 +21,15 @@ class Compilador {
         9   => '=',
         10  => '/',
         11  => '%',
-        12  => 'escreva'
-        // 13 => string
+        12  => 'escreva',
+        // 13 => String,
+        14  => '}',
+        15  => 'inicio',
+        // 16  => Var,
+        17  => '+',
+        18  => '-',
+        19  => '*'
+        // 20  => Number
     ];
 
     public function __construct($code)
@@ -37,39 +44,96 @@ class Compilador {
         $this->tokens = explode("\n", $this->tokens);
         foreach ($this->tokens as $k => $v) $this->tokens[$k] = trim($v);
         $this->tokens = array_filter($this->tokens);
-        foreach ($this->tokens as $k => $v) {
-            if (strpos($v, '"') && strripos($v, '"')) {
-                if (strripos($v, '"') <= strpos($v, '"') || substr_count($v, '"') % 2 != 0) {
-                    trigger_error("Erro na string ('\"')", E_USER_ERROR);
-                } elseif (strripos($v, '"') > strpos($v, '"')) {
-                    array_push($this->tableSerial, [
-                        13 => substr($v, (strpos($v, '"')+1), (strripos($v, '"')-9))
-                        ]);
-                        substr_replace($v, '', 0, 10);
-                }
-            } else {
-                echo array_search($v, $this->reservedWords);
-            }
-        }
-        print_r($this->tokens);
-        return $this->tableSerial;
+        return $this->tokens;
     }
 
     public function createTableSerial()
     {
         $tokens = $this->createTokens();
         foreach ($tokens as $k => $v) {
+            if (strpos($v, '"') && strripos($v, '"')) {
+                if (strripos($v, '"') <= strpos($v, '"') || substr_count($v, '"') % 2 != 0) {
+                    trigger_error("Erro na string (\")", E_USER_ERROR);
+                } elseif (strripos($v, '"') > strpos($v, '"')) {
+                    array_push($this->tableSerial, [
+                        13 => substr($v, (strpos($v, '"')+1), (strripos($v, '"')-9))
+                    ]);
+                    $tokens[$k] = substr_replace($v, '', strpos($v, '"'), strripos($v, '"')-7);
+                    $tokens[$k] = substr_replace($v, '', strpos($v, '('), strripos($v, ')'));
+                    $search = array_search($tokens[$k], $this->reservedWords);
+                    if ($search !== false) {
+                        array_push($this->tableSerial, [
+                            $search => $tokens[$k]
+                        ]);
+                    }
+                }
+            } else {
+                $exp = explode(' ', $v);
+                foreach ($exp as $k => $v) {
+                    $search = array_search($v, $this->reservedWords);
+                    if ($search !== false) {
+                        array_push($this->tableSerial, [
+                            $search => $v
+                        ]);
+                        unset($exp[$k]);
+                    } else {
+                        if (strpos($v, '(') !== false && strripos($v, ')') !== false && strpos($v, '(') < strripos($v, ')')) {
+                            $strF = substr($v, strpos($v, '('), strripos($v, ')'));
+                            if (strpos($v, '(')+1 == strripos($v, ')')) {
+                                $exp[$k] = substr_replace($v, '', strpos($v, '('), strripos($v, ')'));
+                                $search = array_search($exp[$k], $this->reservedWords);
+                                if ($search !== false) {
+                                    array_push($this->tableSerial, [
+                                        $search => $exp[$k]
+                                    ]);
+                                    unset($exp[$k]);
+                                }
+                            } else {
+                                $var = substr($v, strpos($v, '(')+1, strripos($v, ')')-5);
+                                $exp[$k] = substr_replace($v, '', strpos($v, '('), strripos($v, ')'));
+                                array_push($this->tableSerial, [
+                                    16 => $var
+                                ]);
+                                $search = array_search($exp[$k], $this->reservedWords);
+                                if ($search !== false) {
+                                    array_push($this->tableSerial, [
+                                        $search => $exp[$k]
+                                    ]);
+                                    unset($exp[$k]);
+                                }
+                            }
+                        }
+                    }
+                }
 
-            echo "<pre>";
-            print_r ($v);
-            echo "</pre>";
+                foreach ($exp as $k => $v) {
+                    $exp[$k] = str_replace(',', '', $exp[$k]);
+                    if (is_numeric($exp[$k])) {
+                        array_push($this->tableSerial, [
+                            20 => $exp[$k]
+                        ]);
+                        unset($exp[$k]);
+                    }
+                    if (isset($exp[$k]) && !preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $v)) {
+                        array_push($this->tableSerial, [
+                            16 => $exp[$k]
+                        ]);
+                        unset($exp[$k]);
+                    }
+                }
 
+                if (!empty($exp)) {
+                    trigger_error("Um erro de sintaxe foi encontrado", E_USER_ERROR);
+                }
+            }
         }
+
+        return $this->tableSerial;
     }
 
     public function compilar()
     {
-        return $this->createTokens();
+        return $this->createTableSerial();
     }
 
 }
--- index.php
+++ index.php
@@ -11,11 +11,55 @@ $code = 'programa
                 escreva("Digite um valor: ")
                 leia(valor)
 
-                metade_inteira = valor / 2 // Calcula a metade inteira do valor
-                resto = valor % 3 // Calcula o resto da divisão do valor por 3
+                metade_inteira = valor
+                resto = valor % 3
 
                 escreva("\nA metade inteira do numero é: ", metade_inteira)
                 escreva("\nO resto (mod) da divisão por 3 é: ", resto)
+            }
+        }';
+
+$code = 'programa
+        {
+            funcao inicio()
+            {
+                real a, b, soma, sub, mult, div
+
+                escreva("Digite o primeiro número: ")
+                leia(a)
+
+                escreva("Digite o segundo número: ")
+                leia(b)
+
+                soma = a + b
+                sub = a - b
+                mult = a * b
+                div = a / b
+
+                escreva("\nA soma dos números é igual a: ", soma)
+                escreva("\nA subtração dos números é igual a: ", sub)
+                escreva("\nA multiplicação dos números é igual a: ", mult)
+                escreva("\nA divisão dos números é igual a: ", div)
+            }
+        }';
+
+$code = 'programa
+        {
+            funcao inicio()
+            {
+                real resultado
+
+                resultado = 5.0 + 4.0 * 2.0
+                escreva("Operação: 5 + 4 * 2 = ", resultado)
+
+                resultado = 5.0 + 4.0 * 2.0
+                escreva("\nOperação: (5 + 4) * 2 = ", resultado)
+
+                resultado = 1.0 + 2.0 / 3.0 * 4.0
+                escreva("\nOperação: 1 + 2 / 3 * 4 = ", resultado)
+
+                resultado = 1.0 + 2.0 / 3.0 * 4.0
+                escreva("\nOperação: (1 + 2) / (3 * 4) = ", resultado)
             }
         }';
 $compilador = new Compilador($code);
